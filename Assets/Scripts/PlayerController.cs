778db7085e989fd6cdb5c76852645e8a5bc664fa
Philip D'Costa <p.dcosta164@gmail.com>
2018-03-07 22:18:01 -0500

    Added terrain, placeholder character, movement & camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+    Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int rayMaxDistance = 100;
+
+        //Left Mouse Click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if(Physics.Raycast(ray, out hit, rayMaxDistance, movementMask))
+            {
+                //Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                motor.MoveToPoint(hit.point);
+            }
+        }
+
+        //Right Mouse Click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rayMaxDistance))
+            {
+                //Check if hit interactable
+                //If we did, set it as our focus
+            }
+        }
+    }
+}
