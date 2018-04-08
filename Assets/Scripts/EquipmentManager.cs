c9a395d53db2f9a0b2c49314cf7dc7061d07e5a3
Philip D'Costa <p.dcosta164@gmail.com>
2018-04-08 18:54:26 -0400

    Equipments done.
    Added and integrated clothing. Scripted equipement switching between inventory and equipped. Added possibility of default clothing.

---
+++ Assets/Scripts/EquipmentManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour {
+
+    #region Singleton
+    public static EquipmentManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    Equipment[] currentEquipment;
+    Inventory inventory;
+    SkinnedMeshRenderer[] currentMeshes;
+
+    public Equipment[] defaultItems;
+    public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
+    public OnEquipmentChanged onEquipmentChanged;
+    public SkinnedMeshRenderer targetMesh;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+        int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numSlots];
+        currentMeshes = new SkinnedMeshRenderer[numSlots];
+        EquipDefaultItems();
+    }
+
+    public void Equip(Equipment newItem)
+    {
+        int slotIndex = (int) newItem.equipSlot;
+        Equipment oldItem = Unequip(slotIndex);
+
+        if (onEquipmentChanged != null)
+            onEquipmentChanged.Invoke(newItem, oldItem);
+
+        SetEquipmentBlendShapes(newItem, 100);
+        currentEquipment[slotIndex] = newItem;
+
+        SkinnedMeshRenderer newMesh = Instantiate<SkinnedMeshRenderer>(newItem.mesh);
+        newMesh.transform.parent = targetMesh.transform;
+        newMesh.bones = targetMesh.bones;
+        newMesh.rootBone = targetMesh.rootBone;
+        currentMeshes[slotIndex] = newMesh;
+    }
+
+    public Equipment Unequip(int slotIndex)
+    {
+        if(currentEquipment[slotIndex] != null)
+        {
+            if (currentMeshes[slotIndex] != null)
+                Destroy(currentMeshes[slotIndex].gameObject);
+
+            Equipment oldItem = currentEquipment[slotIndex];
+            SetEquipmentBlendShapes(oldItem, 0);
+            inventory.Add(oldItem);
+            currentEquipment[slotIndex] = null;
+
+            if (onEquipmentChanged != null)
+                onEquipmentChanged.Invoke(null, oldItem);
+
+            return oldItem;
+        }
+        return null;
+    }
+
+    public void UnequipAll()
+    {
+        for(int i = 0; i < currentEquipment.Length; i++)
+            Unequip(i);
+        EquipDefaultItems();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+            UnequipAll();
+    }
+
+    //Makes the character skinnier so body mesh doesn't appear over clothing/equipment
+    //Returns to normal when unequipping item
+    void SetEquipmentBlendShapes(Equipment item, int weight)
+    {
+        foreach(EquipmentMeshRegion blendShape in item.coveredMeshRegions)
+            targetMesh.SetBlendShapeWeight((int)blendShape, weight);
+    }
+
+    void EquipDefaultItems()
+    {
+        foreach(Equipment item in defaultItems)
+            Equip(item);
+    }
+}
---
+++ Assets/Scripts/Items/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item {
+
+    public EquipmentSlot equipSlot;
+    public SkinnedMeshRenderer mesh;
+    public EquipmentMeshRegion[] coveredMeshRegions;
+    public int armorModifier;
+    public int damageModifier;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+    }
+}
+
+public enum EquipmentSlot { Head, Chest, Legs, Feet, Weapon, Shield }
+public enum EquipmentMeshRegion { Legs, Arms, Torso };  //Order is important, same as body blendshapes
